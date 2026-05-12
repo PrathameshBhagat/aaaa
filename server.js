d441593c6f3cdbea61eb6eeb8e8263fcf8ca3bc6
PRATHAMESH BHAGAT <90595097+PrathameshBhagat@users.noreply.github.com>
2026-05-12 11:21:57 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ app.use(express.json({ limit: "1mb" }));
 app.post("/run", async (req, res) => {
     const code = req.body.code;
 
+    console.log("Run called on port 3000");
     if (!code) {
         return res.status(400).json({
             error: "No code provided"
