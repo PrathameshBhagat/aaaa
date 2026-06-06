7a930a5bcb2ea6037c52494246cbe8f27bd526f2
PRATHAMESH BHAGAT <90595097+PrathameshBhagat@users.noreply.github.com>
2026-06-06 13:35:13 +0530

    Temp console log in  server.js

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.json({ limit: "1mb" }));
 app.post("/run", async (req, res) => {
     const code = req.body.code;
 
+    console.log("Request " + req.body );
+    
     console.log("Run called on port 3000");
     if (!code) {
         return res.status(400).json({
