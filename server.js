90a579bbe85c6bbb5d3bfaa11f68146a36f9de31
PRATHAMESH BHAGAT <90595097+PrathameshBhagat@users.noreply.github.com>
2026-07-24 18:43:46 +0530

    long change for redis execution

--- server.js
+++ server.js
@@ -22,62 +22,98 @@ app.post("/run", async (req, res) => {
         });
     }
 
-    const id = crypto.randomUUID();
+    try {
 
-    const dir = path.join("/tmp", id);
+        const result = await executeCode(code);
+        return res.json(result);
 
-    fs.mkdirSync(dir, { recursive: true });
+    } catch (err){
 
-    const javaFile = path.join(dir, "Main.java");
+        console.log("Code Execution failed completely in /run ");
+        return res.status(500).json({error:"Code Execution failed completely in /run "});
+    }
 
-    fs.writeFileSync(javaFile, code);
-
-    // javac Main.java && ...............  remove .java too for other mode...
-    const command = `
-        cd "${dir}" &&
-        export LANG=C.UTF-8 &&
-        export LC_ALL=C.UTF-8 &&
-        timeout 15s java -Xmx256m -Dfile.encoding=UTF-8 Main.java
-    `;
-
-    exec(command, (error, stdout, stderr) => {
-
-        fs.rmSync(dir, {
-            recursive: true,
-            force: true
-        });
-
-        if (error) {
-            return res.json({
-                success: false,
-                error: error.message,
-                error_code: error.code,
-                error_signal: error.signal,
-                stderr,
-                stdout
-            });
-        }
-
-        res.json({
-            success: true,
-            stdout,
-            stderr
-        });
-    });
 });
+
+
+function executeCode(code){
+
+    return new Promise((resolve, reject) => {
+        
+        const id = crypto.randomUUID();
+
+        const dir = path.join("/tmp", id);
+
+        fs.mkdirSync(dir, { recursive: true });
+
+        const javaFile = path.join(dir, "Main.java");
+
+        fs.writeFileSync(javaFile, code);
+
+        // javac Main.java && ...............  remove .java too for other mode...
+        const command = `
+            cd "${dir}" &&
+            export LANG=C.UTF-8 &&
+            export LC_ALL=C.UTF-8 &&
+            timeout 15s java -Xmx256m -Dfile.encoding=UTF-8 Main.java
+        `;
+
+        exec(command, (error, stdout, stderr) => {
+
+            fs.rmSync(dir, {
+                recursive: true,
+                force: true
+            });
+
+            if (error) {
+
+                resolve( {
+                    success: false,
+                    error: error.message,
+                    error_code: error.code,
+                    error_signal: error.signal,
+                    stderr,
+                    stdout
+                });
+
+                return;
+            }
+
+            resolve( {
+                success: true,
+                stdout,
+                stderr
+            });
+        });
+    })
+
+    
+}
 
 async function processCodeFromRedisContinously(){
 
     console.log("Worker Started");
 
     while(true){
-        
-        const [, data] = await redis.brpop("jobs", 0);
 
-        const job = JSON.parse(data);
+        try {
 
-        console.log("Recieved Job from redis ID" + job.ID );
-        
+            const [, data] = await redis.brpop("jobs", 0);
+
+            const job = JSON.parse(data);
+
+            console.log("Recieved Job from redis ID" + job.ID );
+
+            const output = await executeCode(job.code);
+
+            await redis.set('job:'+ job.ID , JSON.stringify(output));
+
+            console.log("Executed ID" + job.ID + " OP : " +  JSON.stringify(output));
+
+        } catch (error) {
+            console.error("Error in worker loop" , error);
+        }
+
     }
     
 }
